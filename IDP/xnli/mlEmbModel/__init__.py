3f926dd39369979d03ac5233047958516bb1b687
ImalshaD <plid475@gmail.com>
2025-05-06 00:36:02 +0530

    Add ATMultilingualEmbeddingModel and update module exports

--- IDP/xnli/mlEmbModel/__init__.py
+++ IDP/xnli/mlEmbModel/__init__.py
@@ -2,8 +2,11 @@ from .OFEGEmbeddingModel import OFEGMultilingualEmbeddingModel
 from .SMLBEmbeddingModel import SMLBMultilingualEmbeddingModel
 from .JSMEmbeddingModel import JSMMultilingualEmbeddingModel
 from .LBSMEmbeddingModel import LBSMMultilingualEmbeddingModel
+from .ATEmbeddingModel import ATMultilingualEmbeddingModel
+
 __all__ = ["OFEGMultilingualEmbeddingModel"
            , "SMLBMultilingualEmbeddingModel"
               , "JSMMultilingualEmbeddingModel"
               , "LBSMMultilingualEmbeddingModel"
+              , "ATMultilingualEmbeddingModel"
            ]
